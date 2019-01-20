e5cf59e00229a4aad9a3cb28817de4caab4f1219
codejoncode <jonathanjholloway@gmail.com>
2019-01-19 20:31:43 -0600

    added code for profile controller

---
+++ controller/profile.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+# try something like
+def index(): return dict(message="hello from profile.py")
+
+
+@auth.requires_log()
+def save():
+    profilerows = db(db.profile.created_by==session.auth.user.id).select(orderby=~db.profile.id)
+    for x in profilerow:
+        db.profile.farm_name.default = x.farm_name
+        db.profile.farm_addres.default = x.farm_address
+        db.profile.farm_website.default = x.farm_website
+        break
+    form = SQLFORM(db.profile).process()
+    return locals()
